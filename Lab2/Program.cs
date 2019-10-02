f007a3b73ca6d263aa94f98dbbea83ac1939d9fa
George Lebury <gledbury4@gmail.com>
2019-10-02 15:12:09 -0700

    Work in Progress

---
+++ Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my lab 2 Hello World with embellishments added.");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Uunfortunately I won't be in class on Thursday :(");
+        }
+    }
+}
